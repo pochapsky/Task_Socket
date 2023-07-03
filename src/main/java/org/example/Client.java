edc35490627223fb906085681c704711650c565f
Konstantin Pochapsky <chernoknignikvladikus@yandex.ru>
2023-07-03 17:21:10 +0300

    Edit commit

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -9,7 +9,7 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) {
         String host = "127.0.0.1";
-        int port = 8081;
+        int port = 5000;
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new
                      PrintWriter(clientSocket.getOutputStream(), true);
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,13 +9,19 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int port = 8081;
-        ServerSocket serverSocket = new ServerSocket(port);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        out.println("New connection accepted");
-        final String name = in.readLine();
-        System.out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+        int port = 5000;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Starting");
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    out.println("New connection accepted");
+                    final String name = in.readLine();
+                    System.out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                }
+            }
+        }
     }
 }
